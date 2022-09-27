7d09fc94fa8658c8fcf1d561298b52b3bde7e919
TheWizardo <39444976+TheWizardo@users.noreply.github.com>
2022-09-27 03:00:54 +0300

    show errors

--- src/3-middleware/catch-all.ts
+++ src/3-middleware/catch-all.ts
@@ -3,9 +3,9 @@ import { NextFunction, Request, Response } from "express";
 // catches all errors in the system and sends them as a response
 function catchAll(err: any, req: Request, res: Response, next: NextFunction): void {
     const statusCode = err.status ? err.status : 500;
-    if (statusCode === 500){
-        err.message = "Something went wrong..."
-    }
+    // if (statusCode === 500){
+    //     err.message = "Something went wrong..."
+    // }
     res.status(statusCode).send(err.message);
 }
 
